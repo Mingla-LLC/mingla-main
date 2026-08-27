fe3d61262ebb4e1ac8062755d6d6682dab028d77
Seth Ogieva <sethogievabelgium@gmail.com>
2026-08-27 17:13:19 -0400

    Test reservation calendar venue-time boundaries (#2737)

--- mingla-business/jest.issue2737.render.cjs
+++ mingla-business/jest.issue2737.render.cjs
@@ -4,5 +4,6 @@ module.exports = {
   ...base,
   testMatch: [
     "**/__tests__/reservationCalendar.issue2737.happy.render.test.tsx",
+    "**/__tests__/reservationCalendarModel.issue2737.tester.adversarial.test.ts",
   ],
 };
