2df87528fea3b1bf297bf1d6fd74eab28460c411
Seth Ogieva <sethogievabelgium@gmail.com>
2026-09-02 06:09:37 -0400

    fix(sites): scope version reads to studio tenant (#3035)
    #3035 [version tenant access]

--- mingla-site-cms/src/collections/Navigation.ts
+++ mingla-site-cms/src/collections/Navigation.ts
@@ -1,5 +1,10 @@
 import type { CollectionConfig } from "payload";
-import { enforceLiveStudioWrite, tenantRead, tenantWrite } from "../lib/access";
+import {
+  enforceLiveStudioWrite,
+  tenantRead,
+  tenantVersionRead,
+  tenantWrite,
+} from "../lib/access";
 import { assertTenantPages, singletonPerTenant } from "../lib/tenantIntegrity";
 
 export const Navigation: CollectionConfig = {
@@ -12,7 +17,7 @@ export const Navigation: CollectionConfig = {
     read: tenantRead,
     update: tenantWrite,
     delete: () => false,
-    readVersions: tenantRead,
+    readVersions: tenantVersionRead,
     unlock: tenantWrite,
   },
   versions: { drafts: { autosave: false }, maxPerDoc: 50 },
